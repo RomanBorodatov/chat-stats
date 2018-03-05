6ea895406e741c2000827a4939c8c7f5c8903568
Roman Borodatov <roman.borodatov@gmail.com>
2018-03-05 15:21:15 +0200

    3rd admin color changed

--- src/scripts/communs-bars.js
+++ src/scripts/communs-bars.js
@@ -7,12 +7,14 @@ fetch('all_m_data_adm_msgs001.json')
 		backgroundColors = [
             'rgba(54, 162, 235, 0.2)',
             'rgba(255, 206, 86, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
             'rgba(153, 102, 255, 0.2)',
         	'rgba(255, 159, 64, 0.2)'
         ];
 		borderColors = [
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
+            'rgba(255, 99, 132, 1)',
             'rgba(153, 102, 255, 1)',
             'rgba(255, 159, 64, 1)'
 		];
@@ -55,12 +57,14 @@ fetch('all_m_data_adm_replies001.json')
 		backgroundColors = [
             'rgba(54, 162, 235, 0.2)',
             'rgba(255, 206, 86, 0.2)',
+            'rgba(255, 99, 132, 0.2)',
             'rgba(153, 102, 255, 0.2)',
         	'rgba(255, 159, 64, 0.2)'
         ];
 		borderColors = [
             'rgba(54, 162, 235, 1)',
             'rgba(255, 206, 86, 1)',
+            'rgba(255, 99, 132, 1)',
             'rgba(153, 102, 255, 1)',
             'rgba(255, 159, 64, 1)'
 		];
